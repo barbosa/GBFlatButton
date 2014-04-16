419eb1667eef0afea230bec00a11accbc9cbfb2c
Gustavo Barbosa <gustavocsb@gmail.com>
2014-04-16 17:42:21 -0300

    Updated sample project.

--- Example/GBFlatButtonExample/GBFlatButtonExample/ViewController.m
+++ Example/GBFlatButtonExample/GBFlatButtonExample/ViewController.m
@@ -7,28 +7,51 @@
 //
 
 #import "ViewController.h"
-#import "GBFlatButton.h"
+#import "GBFlatSelectableButton.h"
+#import "UIColor+GBFlatButton.h"
 
 @interface ViewController ()
-{
-    GBFlatButton *button;
-}
+@property (nonatomic, weak) IBOutlet GBFlatButton *pinkButton;
+@property (nonatomic, weak) IBOutlet GBFlatButton *yellowButton;
+@property (nonatomic, weak) IBOutlet GBFlatButton *orangeButton;
+@property (nonatomic, weak) IBOutlet GBFlatButton *greenButton;
+@property (nonatomic, weak) IBOutlet GBFlatSelectableButton *blueButton;
+@property (nonatomic, weak) IBOutlet GBFlatSelectableButton *purpleButton;
 @end
 
 @implementation ViewController
 
-
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-
-    CGRect buttonFrame = CGRectMake(0, 50, 100, 44);
-    button = [[GBFlatButton alloc] initWithFrame:buttonFrame];
-    button.tintColor = [UIColor redColor];
     
-    [button setTitle:@"Flat button" forState:UIControlStateNormal];
+//    GBFlatSelectableButton *blackButton = [[GBFlatSelectableButton alloc] initWithFrame:CGRectMake(10, 30, 150, 44)];
+//    blackButton.tintColor = [UIColor blackColor];
+//    [blackButton setTitle:@"Black"
+//                 forState:UIControlStateNormal];
+//    [self.view addSubview:blackButton];
     
-//    [self.view addSubview:button];
+    _pinkButton.tintColor = [UIColor gb_pinkColor];
+    _pinkButton.layer.cornerRadius = 5.0f;
+    
+    _yellowButton.tintColor = [UIColor gb_yellowColor];
+    
+    _orangeButton.tintColor = [UIColor orangeColor];
+    _orangeButton.selected = YES;
+    
+    _greenButton.tintColor = [UIColor gb_greenColor];
+    
+    _blueButton.tintColor = [UIColor gb_blueColor];
+    _blueButton.selected = YES;
+    _blueButton.layer.cornerRadius = 8.0f;
+    [_blueButton setTitle:@"Blue selected"
+                 forState:UIControlStateSelected];
+    
+    _purpleButton.tintColor = [UIColor gb_purpleColor];
+    [_purpleButton setTitle:@"Selectable button"
+                   forState:UIControlStateNormal];
+    [_purpleButton setTitle:@"Yay! Button selected!"
+                   forState:UIControlStateSelected];
 }
 
 @end
